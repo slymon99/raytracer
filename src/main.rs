212f3294c25dda7492204a5c2f64619019c252ff
Simon Clark <simonclark236@gmail.com>
2020-08-19 18:17:55 -0400

    simple ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Write;
+
+fn main() -> std::io::Result<()> {
+    const WIDTH: u32 = 512;
+    const HEIGHT: u32 = 512;
+
+    let mut out = File::create("img.ppm")?;
+    out.write(format!("P3\n{} {}\n255\n",WIDTH, HEIGHT).as_bytes())?;
+
+    for row in 0..HEIGHT {
+        eprintln!("Lines remaining {}", HEIGHT-row);
+        for col in 0..WIDTH {
+            out.write(format!("{} {} {}\n", row%255, col%255, 255).as_bytes());
+        }
+    }
+    Ok(())
 }
