bd21637067b9941c06eb5c5235444f1af08cf13d
Simon Clark <simonclark236@gmail.com>
2020-08-21 16:42:19 -0400

    vecs

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,6 +1,8 @@
-use std::ops;
+use std::{ops, fmt};
 use std::ops::{Add, Mul};
+use std::fmt::Formatter;
 
+#[derive(PartialEq, Debug)]
 struct Vec3 {
     x: f64,
     y: f64,
@@ -13,7 +15,7 @@ impl Vec3 {
     }
 
     pub fn length_square(self) -> f64 {
-        x * x + y * y + z * z
+        self.x * self.x + self.y * self.y + self.z * self.z
     }
     pub fn length(self) -> f64 {
         self.length_square().sqrt()
@@ -25,6 +27,14 @@ impl ops::Add<Vec3> for Vec3 {
 
     fn add(self, rhs: Vec3) -> Self::Output {
         Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
     }
 }
 
@@ -40,6 +50,22 @@ impl ops::Div<f64> for Vec3 {
     type Output = Vec3;
 
     fn div(self, rhs: f64) -> Self::Output {
-        self.mul(1 / rhs)
+        self.mul(1.0 / rhs)
+    }
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {}", self.x, self.y, self.z)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vec3::Vec3;
+
+    #[test]
+    fn addition() {
+        assert_eq!(Vec3::new(1.0, 2.0, 3.0) + Vec3::new(4.0, 5.0, 6.0), Vec3::new(5.0, 7.0, 9.0));
     }
 }
