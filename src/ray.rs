bb6e444e5957717ac197be8c84e43e35bad4a035
Simon Clark <simonclark236@gmail.com>
2020-08-25 18:29:34 -0400

    reference refactoring and simplify ops with impl_ops crate

--- src/ray.rs
+++ src/ray.rs
@@ -2,16 +2,16 @@ use crate::vec3::Vec3;
 
 #[derive(Debug, PartialOrd, PartialEq)]
 pub struct Ray {
-    origin: Vec3,
-    direction: Vec3
+    pub origin: Vec3,
+    pub direction: Vec3
 }
 
 impl Ray {
     pub fn new(origin: Vec3, direction: Vec3) -> Ray {
         Self {origin, direction}
     }
-    pub fn at(self, t: f64) -> Vec3 {
-        return self.origin + self.direction * t;
+    pub fn at(&self, t: f64) -> Vec3 {
+        return &self.origin + &(&self.direction * t);
     }
 }
 
@@ -22,7 +22,7 @@ mod tests {
 
     #[test]
     fn at() {
-        let ray: Ray = Ray::new(Vec3::new(1.0, 1.0, 1.0), Vec3::new(1.0, 1.0, 0.0));
+        let ray: &Ray = &Ray::new(Vec3::new(1.0, 1.0, 1.0), Vec3::new(1.0, 1.0, 0.0));
         assert_eq!(ray.at(5.0), Vec3::new(6.0, 6.0, 1.0));
     }
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,12 +1,11 @@
 use std::{ops, fmt};
-use std::ops::{Add, Mul};
 use std::fmt::Formatter;
 
 #[derive(PartialOrd, PartialEq, Debug)]
 pub struct Vec3 {
-    x: f64,
-    y: f64,
-    z: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 impl Vec3 {
@@ -14,45 +13,26 @@ impl Vec3 {
         Self {x, y, z}
     }
 
-    pub fn length_square(self) -> f64 {
+    pub fn length_square(&self) -> f64 {
         self.x * self.x + self.y * self.y + self.z * self.z
     }
-    pub fn length(self) -> f64 {
+    pub fn length(&self) -> f64 {
         self.length_square().sqrt()
     }
-}
-
-impl ops::Add<Vec3> for Vec3 {
-    type Output = Vec3;
-
-    fn add(self, rhs: Vec3) -> Self::Output {
-        Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    pub fn unit(&self) -> Vec3 {
+        self / self.length()
     }
 }
 
-impl ops::Sub<Vec3> for Vec3 {
-    type Output = Vec3;
+impl_op_ex!(+ |a: &Vec3, b: &Vec3| -> Vec3 { Vec3::new(a.x + b.x, a.y + b.y, a.z + b.z)});
+impl_op_ex!(- |a: &Vec3, b: &Vec3| -> Vec3 { Vec3::new(a.x - b.x, a.y - b.y, a.z - b.z)});
+impl_op_ex!(* |a: &Vec3, b: &Vec3| -> Vec3 { Vec3::new(a.x * b.x, a.y * b.y, a.z * b.z)});
+impl_op_ex!(/ |a: &Vec3, b: &Vec3| -> Vec3 { Vec3::new(a.x / b.x, a.y / b.y, a.z / b.z)});
 
-    fn sub(self, rhs: Vec3) -> Self::Output {
-        Vec3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
-    }
-}
-
-impl ops::Mul<f64> for Vec3 {
-    type Output = Vec3;
-
-    fn mul(self, rhs: f64) -> Self::Output {
-        Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs)
-    }
-}
-
-impl ops::Div<f64> for Vec3 {
-    type Output = Vec3;
-
-    fn div(self, rhs: f64) -> Self::Output {
-        self.mul(1.0 / rhs)
-    }
-}
+impl_op_ex_commutative!(+ |a: &Vec3, b: f64| -> Vec3 { Vec3::new(a.x + b, a.y + b, a.z + b)});
+impl_op_ex_commutative!(- |a: &Vec3, b: f64| -> Vec3 { Vec3::new(a.x - b, a.y - b, a.z - b)});
+impl_op_ex_commutative!(* |a: &Vec3, b: f64| -> Vec3 { Vec3::new(a.x * b, a.y * b, a.z * b)});
+impl_op_ex_commutative!(/ |a: &Vec3, b: f64| -> Vec3 { Vec3::new(a.x / b, a.y / b, a.z / b)});
 
 impl fmt::Display for Vec3 {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
@@ -66,6 +46,10 @@ mod tests {
 
     #[test]
     fn addition() {
-        assert_eq!(Vec3::new(1.0, 2.0, 3.0) + Vec3::new(4.0, 5.0, 6.0), Vec3::new(5.0, 7.0, 9.0));
+        assert_eq!(&Vec3::new(1.0, 2.0, 3.0) + &Vec3::new(4.0, 5.0, 6.0), Vec3::new(5.0, 7.0, 9.0));
+    }
+
+    fn addition_floats() {
+        assert_eq!(Vec3::new(1.0, 2.0, 3.0) / 2.0, Vec3::new(0.5, 1.0, 1.5));
     }
 }
